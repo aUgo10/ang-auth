d16cda793fd3600e2a44cef417e85b522b7a95a1
Anthony Ugo <anthonyugo@gmail.com>
2020-02-20 16:22:56 -0600

    First push

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+// 1. Import HttpHeaders, this will allow us to POST JSON data
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { User } from '../models/users';
+
+// 2. Create a JSON header
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  // 3. Add URL as an instance variable
+  private url:string = 'http://localhost:3000/api/auth';
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  // 4. Change test() to login() and accept a user object as an argument
+  // 5. Expect a User object instead of an array of User objects
+  // 6. Change from a GET to a POST request
+  // 7. Pass the user object and the HTTP headers into the POST request
+  login(user: User): Observable<User> {
+    return this.http.post<User>(`${this.url}/login`, user, httpOptions);
+  }
+}
